8df0fe9d9088c40562e9695e0cb8f6302c9b794f
Szatmári Mihály <szatmarr.mihalyy@gmail.com>
2025-12-12 19:51:49 +0100

    fix v.0.1

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,6 +3,8 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle"
 import { db } from "@/db"  
 
 export const auth = betterAuth({
+    appUrl : process.env.BETTER_AUTH_URL!,
+    secret : process.env.BETTER_AUTH_SECRET!,
     database : drizzleAdapter(db, {
         provider : 'pg'
     }),
